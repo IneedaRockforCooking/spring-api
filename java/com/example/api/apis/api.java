fc159a773886f1f3fc8f9f117f9b2ffeaad46558
IneedaRockforCooking <132123729+IneedaRockforCooking@users.noreply.github.com>
2023-06-08 06:02:33 -0400

    Create api.java

---
+++ java/com/example/api/apis/api.java
@@ -0,0 +1,27 @@
+package com.example.api.apis;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.*;
+
+import java.util.List;
+
+@Document(collection="uu")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Api {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String releaseDate;
+
+    public Comic(String comicId, String title, String releaseDate) {
+        this.comicId = comicId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+    }
+}
